fc1bb70bef469166a58a0e246be1aeb0a58cc8cf
vibhor <goyal1992.vibhor@gmail.com>
2020-08-26 23:11:04 +0530

    added Tier 1-2 cities in an array

--- actions.py
+++ actions.py
@@ -2,34 +2,53 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
-import zomatopy
 import json
 
-class ActionSearchRestaurants(Action):
-	def name(self):
-		return 'action_search_restaurants'
-		
-	def run(self, dispatcher, tracker, domain):
-		config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
-		zomato = zomatopy.initialize_app(config)
-		loc = tracker.get_slot('location')
-		cuisine = tracker.get_slot('cuisine')
-		location_detail=zomato.get_location(loc, 1)
-		d1 = json.loads(location_detail)
-		lat=d1["location_suggestions"][0]["latitude"]
-		lon=d1["location_suggestions"][0]["longitude"]
-		cuisines_dict={'bakery':5,'chinese':25,'cafe':30,'italian':55,'biryani':7,'north indian':50,'south indian':85}
-		results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
-		d = json.loads(results)
-		response=""
-		if d['results_found'] == 0:
-			response= "no results"
-		else:
-			for restaurant in d['restaurants']:
-				response=response+ "Found "+ restaurant['restaurant']['name']+ " in "+ restaurant['restaurant']['location']['address']+"\n"
-		
-		dispatcher.utter_message("-----"+response)
-		return [SlotSet('location',loc)]
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
 
+import zomatopy
+
+
+class ActionSearchRestaurants(Action):
+    def name(self):
+        return 'action_search_restaurants'
+
+    def run(self, dispatcher, tracker, domain):
+        eligible_cities = [
+            'Ahmedabad', 'Bengaluru', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai', 'Pune',
+            'Agra', 'Ajmer', 'Aligarh', 'Amravati', 'Amritsar', 'Asansol', 'Aurangabad', 'Bareilly', 'Belgaum',
+            'Bhavnagar', 'Bhiwandi', 'Bhopal', 'Bhubaneswar', 'Bikaner', 'Bilaspur', 'Bokaro Steel City', 'Chandigarh',
+            'Coimbatore', 'Cuttack', 'Dehradun', 'Dhanbad', 'Bhilai', 'Durgapur', 'Dindigul', 'Erode', 'Faridabad',
+            'Firozabad', 'Ghaziabad', 'Gorakhpur', 'Gulbarga', 'Guntur', 'Gwalior', 'Gurgaon', 'Guwahati', 'Hamirpur',
+            'Hubli–Dharwad', 'Indore', 'Jabalpur', 'Jaipur', 'Jalandhar', 'Jammu', 'Jamnagar', 'Jamshedpur', 'Jhansi',
+            'Jodhpur', 'Kakinada', 'Kannur', 'Kanpur', 'Kochi', 'Kolhapur', 'Kollam', 'Kozhikode', 'Kurnool',
+            'Ludhiana', 'Lucknow', 'Madurai', 'Malappuram', 'Mathura', 'Mangalore', 'Meerut', 'Moradabad', 'Mysore',
+            'Nagpur', 'Nanded', 'Nashik', 'Nellore', 'Noida', 'Patna', 'Pondicherry', 'Purulia', 'Prayagraj', 'Raipur',
+            'Rajkot', 'Rajahmundry', 'Ranchi', 'Rourkela', 'Salem', 'Sangli', 'Shimla', 'Siliguri', 'Solapur',
+            'Srinagar', 'Surat', 'Thanjavur', 'Thiruvananthapuram', 'Thrissur', 'Tiruchirappalli', 'Tirunelveli',
+            'Ujjain', 'Bijapur', 'Vadodara', 'Varanasi', 'Vasai-Virar City', 'Vijayawada', 'Visakhapatnam', 'Vellore',
+            'Warangal'
+        ]
+        config = {"user_key": "9f60f6d560754fcd24e4a93ca3b6f501"}
+        zomato = zomatopy.initialize_app(config)
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
+        location_detail = zomato.get_location(loc, 1)
+        d1 = json.loads(location_detail)
+        lat = d1["location_suggestions"][0]["latitude"]
+        lon = d1["location_suggestions"][0]["longitude"]
+        cuisines_dict = {'bakery': 5, 'chinese': 25, 'cafe': 30, 'italian': 55, 'biryani': 7, 'north indian': 50,
+                         'south indian': 85}
+        results = zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+        d = json.loads(results)
+        response = ""
+        if d['results_found'] == 0:
+            response = "no results"
+        else:
+            for restaurant in d['restaurants']:
+                response = response + "Found " + restaurant['restaurant']['name'] + " in " + \
+                           restaurant['restaurant']['location']['address'] + "\n"
+
+        dispatcher.utter_message("-----" + response)
+        return [SlotSet('location', loc)]
